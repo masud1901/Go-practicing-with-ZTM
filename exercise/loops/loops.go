01639242d8687ec1e6d7952e2c544c51aba5ff9e
masud1901 <akmolmasud5@gmail.com>
2025-04-09 02:42:09 +0600

    looops practiced

--- exercise/loops/loops.go
+++ exercise/loops/loops.go
@@ -12,7 +12,27 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+func fizzBuss(number int) string {
+	if number%3 == 0 && number%5 == 0 {
+		return "FizzBuzz"
+	} else if number%3 == 0 {
+		return "Fizz"
+
+	} else if number%5 == 0 {
+		return "Buzz"
+	}
+
+	return strconv.Itoa(number)
+}
 
 func main() {
+
+	for i := 1; i <= 50; i++ {
+		fmt.Println(fizzBuss(i))
+	}
 }
