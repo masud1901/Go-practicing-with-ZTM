66508a4efdfb77c09388c1768ddf1f6e51c724a5
masud1901 <akmolmasud5@gmail.com>
2025-04-26 18:02:51 +0600

    practiced maps

--- demo/maps/maps.go
+++ demo/maps/maps.go
@@ -3,4 +3,37 @@ package main
 import "fmt"
 
 func main() {
+	shoppingList := make(map[string]int)
+
+	shoppingList["eggs"] = 11
+	shoppingList["milk"] = 34
+	shoppingList["bread"] = 11
+	shoppingList["bread"] += 11
+
+	shoppingList["egg"] += 1
+	fmt.Println(shoppingList)
+
+	delete(shoppingList, "milk")
+
+	fmt.Println("Milk deleted, new list:", shoppingList)
+
+	fmt.Println("need", shoppingList["eggs"], "eggs")
+
+	cereal, found := shoppingList["cereal"]
+
+	fmt.Println("Need cereal?")
+
+	if !found {
+		fmt.Println("nope")
+	} else {
+		fmt.Println("yup", cereal, "boxes")
+	}
+
+	totalItems := 0
+
+	for _, amount := range shoppingList {
+		totalItems += amount
+	}
+
+	fmt.Println("There are", totalItems, "items on the shopping list")
 }
--- exercise/maps/maps.go
+++ exercise/maps/maps.go
@@ -2,22 +2,22 @@
 //  Write a program to display server status.
 //
 //--Requirements:
-//* Create a function to print server status, including:
-//  - Number of servers
-//  - Number of servers for each status (Online, Offline, Maintenance, Retired)
+
 //* Store the existing slice of servers in a map
 //* Default all of the servers to `Online`
 //* Perform the following status changes and display server info:
 //  - display server info
 //  - change `darkstar` to `Retired`
-//  - change `aiur` to `Offline`
+//  - change `aiur` to `Offline
 //  - display server info
 //  - change all servers to `Maintenance`
 //  - display server info
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const (
 	Online      = 0
@@ -26,6 +26,57 @@ const (
 	Retired     = 3
 )
 
+// * Create a function to print server status, including:
+//   - Number of servers
+//   - Number of servers for each status (Online, Offline, Maintenance, Retired)
+func printServer(servers map[string]int) {
+	fmt.Println("\nThere are", len(servers), "servers")
+
+	stats := make(map[int]int)
+
+	for _, status := range servers {
+		switch status {
+		case Online:
+			stats[Online] += 1
+		case Offline:
+			stats[Offline] += 1
+		case Maintenance:
+			stats[Maintenance] += 1
+		case Retired:
+			stats[Retired] += 1
+
+		default:
+			panic("unhandled server status")
+
+		}
+
+	}
+
+	fmt.Println(stats[Online], "servers are online")
+	fmt.Println(stats[Offline], "servers are offline")
+	fmt.Println(stats[Maintenance], "servers are undergoing maintainance")
+	fmt.Println(stats[Retired], "servers are retired")
+}
+
 func main() {
 	servers := []string{"darkstar", "aiur", "omicron", "w359", "baseline"}
+
+	serverList := make(map[string]int)
+
+	for _, server := range servers {
+		serverList[server] = Online
+	}
+
+	printServer(serverList)
+
+	serverList["darkstar"] = Retired
+	serverList["aiur"] = Offline
+
+	printServer(serverList)
+
+	for server, _ := range serverList {
+		serverList[server] = Maintenance
+	}
+	printServer(serverList)
+
 }
