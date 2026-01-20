76697ca621b88dd12f65726ed2b194dea65e596f
masud1901 <akmolmasud5@gmail.com>
2026-01-21 00:36:18 +0600

    Add parking lot management and player statistics functionality

--- demo/rcv-func/rcv-func.go
+++ demo/rcv-func/rcv-func.go
@@ -2,6 +2,43 @@ package main
 
 import "fmt"
 
+type Space struct {
+	occupied bool
+}
+
+type ParkingLot struct {
+	spaces []Space
+}
+
+func occupySpace(lot *ParkingLot, spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *ParkingLot) occupySpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *ParkingLot) freeSpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = false
+}
+
 func main() {
+	lot := ParkingLot{
+		spaces: make([]Space, 5),
+	}
+	fmt.Println("Initial parking lot state:")
+	for i, space := range lot.spaces {
+		fmt.Printf("Space %d occupied: %t\n", i+1, space.occupied)
+	}
+
+	fmt.Println("\nAfter occupying spaces 2 and 3, and freeing space 2:")
+	occupySpace(&lot, 2)
+
+	lot.occupySpace(3)
+	lot.freeSpace(2)
+
+	for i, space := range lot.spaces {
+		fmt.Printf("Space %d occupied: %t\n", i+1, space.occupied)
+	}
 
 }
--- exercise/rcv-func/rcv-func.go
+++ exercise/rcv-func/rcv-func.go
@@ -16,6 +16,49 @@ package main
 
 import "fmt"
 
+
+type player struct {
+	health    int
+	maxHealth int
+	energy    int
+	maxEnergy int
+	name      string
+}
+
+func (p *player) statistic()  {
+	fmt.Println("Player:", p.name)
+	fmt.Println("Health:", p.health, "/", p.maxHealth)
+	fmt.Println("Energy:", p.energy, "/", p.maxEnergy)
+}
+
+func (p *player) damageTaken(damage int)  {
+	p.health -= damage
+	if p.health < 0 {
+		p.health = 0
+	}
+	fmt.Printf("%s took %d damage! Current health: %d/%d\n", p.name, damage, p.health, p.maxHealth)
+}
+
+func (p *player) energyUsed(energy int)  {
+	p.energy -= energy
+	if p.energy < 0 {
+		p.energy = 0
+	}
+	fmt.Printf("%s used %d energy! Current energy: %d/%d\n", p.name, energy, p.energy, p.maxEnergy)
+}
+
+
 func main() {
 
+	player1 := player{
+		health:    100,
+		maxHealth: 100,
+		energy:    50,
+		maxEnergy: 50,
+		name:      "Masud",
+	}
+	player1.statistic()
+	player1.damageTaken(30)
+	player1.energyUsed(20)
+	player1.statistic()
 }
